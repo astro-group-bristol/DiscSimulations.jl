93794de183b4ea6868882046a9ef78a488336ea3
Alice <nj19721@bristol.ac.uk>
2023-03-13 17:42:16 +0000

    Fiddled around and added non-periodic boundary conditions

--- simpleGravitySim/simpleGravity.jl
+++ simpleGravitySim/simpleGravity.jl
@@ -7,7 +7,24 @@ struct gravEq1D <: Trixi.AbstractEquations{1 #= number of spatial dimensions =#,
 end
 
 @inline Trixi.flux(u, orientation, equation::gravEq1D) = SVector(0, -u[2])
+#@inline Trixi.flux(u, orientation, equation::gravEq1D) = u
 Trixi.varnames(::typeof(cons2cons), ::gravEq1D) = ("phi","q1")
+
+#struct BoundaryConditionConstantDirichlet{T <: Real}
+#    boundary_value::T
+#end
+
+#@inline function (boundary_condition::BoundaryConditionConstantDirichlet)(u, normal::AbstractVector,
+#    x, t, operator_type::Trixi.Gradient,
+#    equation::gravEq1D)
+#return boundary_condition.boundary_value
+#end
+
+#@inline function (boundary_condition::BoundaryConditionConstantDirichlet)(u, normal::AbstractVector,
+#    x, t, operator_type::Trixi.Divergence,
+#    equation::gravEq1D)
+#return u[2]
+#end
 
 end # module
 
--- simpleGravitySim/simulation.jl
+++ simpleGravitySim/simulation.jl
@@ -13,7 +13,7 @@ function build_initial_conditions(user_supplied_ρ::Function)
         # call user supplied function
         ρ = user_supplied_ρ(x, t, equations)
         #using the acceleration calculation in the og paper's setup for now
-        q1 = -2*π*sin(π*x[1])
+        q1 = -2*π*sin(π * x[1])
         return SVector(ρ,q1)
     end
     # closures are captured, so return the function to use in trixi
@@ -22,16 +22,19 @@ end
 
 #set up solver for equations
 function setSolver(initCond, equations, mesh)
+    boundary_conditions = (x_neg=BoundaryConditionDirichlet(((x, t, equations) -> SVector(sin(x[1]), -2*π*sin(π * x[1])))),
+                       x_pos=BoundaryConditionDirichlet(((x, t, equations) -> SVector(sin(x[1]), -2*π*sin(π * x[1])))))
     solver = DGSEM(3, flux_central)
-    semi = SemidiscretizationHyperbolic(mesh, equations, initCond, solver)
-    tspan = (0.0, 0.1)
+    semi = SemidiscretizationHyperbolic(mesh, equations, initCond, solver, boundary_conditions=boundary_conditions)
+    tspan = (0.0, 1.0)
     ode = semidiscretize(semi, tspan)
     return ode
 end
 
 #run simulations and collect results
 function runAnimation(ode)
-    sol = @time solve(ode, RDPK3SpFSAL49(), abstol=1.0e-7, reltol=1.0e-7)
+    callbacks = CallbackSet(SummaryCallback())
+    sol = @time solve(ode, RDPK3SpFSAL49(), abstol=1.0e-7, reltol=1.0e-7, callback=callbacks)
     tspan = collect(range(0.0, 1.0, 60))
     frames = Plots.@animate for t in tspan
         p = plot(sol(t))
@@ -49,6 +52,7 @@ function main(user_supplied_ρ::Function)
     t0 = 0
     mesh = TreeMesh(-1.0, 1.0, # min/max coordinates
                 initial_refinement_level=4,
+                periodicity=false,
                 n_cells_max=10^4)
 
     #get initial conditions function and run
@@ -61,10 +65,15 @@ function main(user_supplied_ρ::Function)
     runAnimation(ode)
 end
 
-initial_condition_sine(x, t, equation::simpleGravity.gravEq1D) = sinpi(x[1])
+#from 3.1.1 of the paper. doesn't matter but thought i'd use it
+#initial_condition_sine(x, t, equation::simpleGravity.gravEq1D) = 2 + (1/10) * sin(π*(x[1] - t))
+initial_condition_sine(x, t, equation::simpleGravity.gravEq1D) = sin(x[1])
 
 #other initial condition options:
-#initial_condition_cos(x, t, equation::simpleGravity.gravEq1D) = 2 + 2*cos(π*x[1])
+#initial_condition = (x, t, equations) -> SVector(0.0)
+#initial_condition_cos(x, t, equation::simpleGravity.gravEq1D) = SVector(2 + 2*cos(π*x[1]))
 #initial_condition_x(x, t, equation::simpleGravity.gravEq1D) = x[1]
+
+#main(initial_condition)
 
 main(initial_condition_sine)
