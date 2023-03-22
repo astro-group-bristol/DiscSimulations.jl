b0cfcd36e76a5a6f07daf8a3ee60112cc0af5740
fjebaker <fergusbkr@gmail.com>
2023-03-22 15:45:34 +0000

    fix: use new solutions type in example

--- examples/burger-chef.jl
+++ examples/burger-chef.jl
@@ -14,15 +14,19 @@ simple_sol = @time solve(simple_problem, Rodas5(autodiff=false); reltol = 1e-7, 
 spectral_x, _, T, Ti, spectral_problem = BurgerSpectral.setup(N, xmax)
 spectral_sol = @time solve(spectral_problem, Rodas5(autodiff=false); reltol = 1e-7, abstol = 1e-7)
 
-trixi_x, trixi_problem = BurgerTrixi.setup(N, xmax)
-trixi_sol = @time solve(trixi_problem, RDPK3SpFSAL49(), abstol=1.0e-7, reltol=1.0e-7)
+# trixi_x, trixi_problem = BurgerTrixi.setup(N, xmax)
+# trixi_sol = @time solve(trixi_problem, RDPK3SpFSAL49(), abstol=1.0e-7, reltol=1.0e-7)
 
+# use the new disc solution type
+discsol = BurgerTrixi.solve_disc(N, xmax)
+# plot that individually
+DiscSimulations.plotgif(discsol, 0.0, 1.0)
 
 tspan = collect(range(0.0, 1.0, 60))
 frames = Plots.@animate for t in tspan
     p = plot()
 
-    plot!(trixi_sol(t), trixi_x, label="Trixi")
+    # plot!(trixi_sol(t), trixi_x, label="Trixi")
     plot!(simple_x, simple_sol(t), label="Simple")
     plot!(spectral_x, Ti * spectral_sol(t), label="Spectral")
 
